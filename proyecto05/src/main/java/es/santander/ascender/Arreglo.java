8f445857ccf9b5578a9b4dfe75753dc5fbd57ff1
curso15 <curso15@gmail.com>
2024-11-11 13:01:46 +0100

    nuevo

--- proyecto05/src/main/java/es/santander/ascender/Arreglo.java
+++ proyecto05/src/main/java/es/santander/ascender/Arreglo.java
@@ -2,4 +2,31 @@ package es.santander.ascender;
 
 public class Arreglo {
 
+    public int buscarMayor(int [] arregloNuevo){
+
+    
+      for(int i = 0; i < arregloNuevo.length; i ++){
+
+        if(arregloNuevo[i] > arregloNuevo[i+1]){
+            int mayor = arregloNuevo[i];
+        }
+      }
+
+      public int buscarMenor( int [] arregloNuevo){
+
+      
+
+        for(int i = 0; i < arregloNuevo.length; i ++){
+
+            if(arregloNuevo[i] > menor){
+                mayor = arregloNuevo[i];
+            }
+          }
+          return mayor;
+
+
+      }
+
+    }
+
 }
