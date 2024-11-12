137286a5379e014eedf5c868fd2ca1c4865f9d35
curso00 <curso00@nomail.com>
2024-11-12 10:54:56 +0100

    Arreglamos parte de los problemas, los test pasan

--- proyecto05/src/main/java/es/santander/ascender/Arreglo.java
+++ proyecto05/src/main/java/es/santander/ascender/Arreglo.java
@@ -4,65 +4,38 @@ import java.util.Random;
 
 public class Arreglo {
 
+  // Creo un objeto de la clase random
+  private Random aleatorio = new Random();
 
- 
+  public int buscarMayor(int[] arregloNuevo) {
+    int mayor = arregloNuevo[0];
 
-    public int buscarMayor(int [] arregloNuevo){
-
-       int mayor = arregloNuevo[0];
-
-      for(int i = 1; i < arregloNuevo.length; i ++){
-        
-        
-        if(arregloNuevo[i] > mayor){
-
-            mayor = arregloNuevo[i];
-        }
+    for (int i = 1; i < arregloNuevo.length; i++) {
+      if (arregloNuevo[i] > mayor) {
+        mayor = arregloNuevo[i];
       }
-        
-      return mayor; 
-      
-       }
+    }
+    return mayor;
+  }
 
+  public int buscarMenor(int[] arregloNuevo) {
+    int menor = arregloNuevo[0];
 
-
-      public int buscarMenor( int [] arregloNuevo){
-
-      int menor = arregloNuevo[0];
-
-        for(int i = 1; i < arregloNuevo.length; i ++){
-
-            if(arregloNuevo[i] < menor){
-
-                menor  = arregloNuevo[i];
-            }
-          }
-          return menor;
-
+    for (int i = 1; i < arregloNuevo.length; i++) {
+      if (arregloNuevo[i] < menor) {
+        menor = arregloNuevo[i];
       }
-      
-      // Creo un objeto de la clase random
+    }
+    return menor;
+  }
 
-      Random aleatorio = new Random();
-      
+  public int[] obtenerNumerosRandom(int valor) {
+    int[] arrAleatorio = new int[valor];
 
-      public int [] obtenerNumerosRandom( int valor){
+    for (int i = 0; i < valor; i++) {
+      arrAleatorio[i] = aleatorio.nextInt();
+    }
 
-        int[] arrAleatorio = new int[valor];
-
-        for (int i = 0; i <= valor; i ++){
-
-            arrAleatorio[i] = aleatorio.nextInt();
-
-        }
-
-        return arrAleatorio; 
-
-      }
-    
-      }
-
-
-
-
-
+    return arrAleatorio;
+  }
+}
--- proyecto05/src/test/java/es/santander/ascender/ArregloTest.java
+++ proyecto05/src/test/java/es/santander/ascender/ArregloTest.java
@@ -4,51 +4,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-
-
-
 public class ArregloTest {
-
 
     @Test
     public void testBuscarMayor(){
-
         Arreglo cp = new Arreglo();
-
         int[] array = {5, 9 , 1 , 6};
 
         int mayor = cp.buscarMayor(array);
 
         assertEquals(9, mayor);
-
     }
 
     @Test
     public void testBuscarMenor(){
-
         Arreglo ki = new Arreglo();
-
         int[] arr = {5, - 5 , 1 , 6};
 
         int menor = ki.buscarMenor(arr);
 
         assertEquals(-5, menor);
-
     }
 
     @Test 
     public void testObtenerNumeroRandom(){
-
         Arreglo al = new Arreglo();
 
         int[] lista = al.obtenerNumerosRandom(5);
 
         for( int elemento : lista){
-   
             System.out.println(elemento + "");
-
         }     
     }
-     
-
 }
