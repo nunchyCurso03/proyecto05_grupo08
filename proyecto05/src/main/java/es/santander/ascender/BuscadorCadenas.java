a13d7368f444dba48e1b32cc1d4027ccdee742db
curso00 <curso00@nomail.com>
2024-11-13 10:44:16 +0100

    Primera implementación del método de cadenas duplicadas, pero faltan detalles

--- proyecto05/src/main/java/es/santander/ascender/BuscadorCadenas.java
+++ proyecto05/src/main/java/es/santander/ascender/BuscadorCadenas.java
@@ -2,7 +2,28 @@ package es.santander.ascender;
 
 public class BuscadorCadenas {
     public String[] localizarComunes(String[] primeraLista, 
-                                        String[] segundaLista) {
-        throw new UnsupportedOperationException("No implementado aun");
+    String[] segundaLista) {
+        
+        int numeroDeCoincidenciasTotales = 0;
+        for (int i = 0; i < primeraLista.length; i++) {
+            for (int j = 0; j < segundaLista.length; j++) {
+                if ( primeraLista[i].equals(segundaLista[j]) ) {
+                    numeroDeCoincidenciasTotales++;
+                }
+            }
+        }
+
+        String[] resultado = new String[numeroDeCoincidenciasTotales];
+
+        int cuentosRepetidosPorAhora = 0;
+        for (int i = 0; i < primeraLista.length; i++) {
+            for (int j = 0; j < segundaLista.length; j++) {
+                if ( primeraLista[i].equals(segundaLista[j]) ) {
+                    resultado[cuentosRepetidosPorAhora] = primeraLista[i];
+                    cuentosRepetidosPorAhora++;
+                }
+            }
+        }
+        return resultado;
     }
 }
